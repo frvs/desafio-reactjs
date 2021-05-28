4caee2422a469bad0d2685184ceaf47854d033f5
Lucas Frois <lucas.frois@lambda3.com.br>
2021-05-27 21:28:18 -0300

    add user page schema

--- app/src/pages/User/User.tsx
+++ app/src/pages/User/User.tsx
@@ -1,3 +1,40 @@
-const User = () => <div>user</div>
+const User = () => (
+  <div>
+    <section>
+      <img alt="perfil"></img>
+      <p>description</p>
+      <span>
+        <p>followers</p>
+        <p>following</p>
+        <p>stars</p>
+      </span>
+      <div>
+        <p>organization</p>
+      </div>
+      <div>
+        <p>location</p>
+      </div>
+      <div>
+        <p>email</p>
+      </div>
+      <div>
+        <p>website</p>
+      </div>
+      <div>
+        <p>twitter</p>
+      </div>
+    </section>
+    <section>
+      <div>
+        <h1>Repository name</h1>
+        <p>repository description</p>
+        <span>
+          <p>stars</p>
+          <p>last update</p>
+        </span>
+      </div>
+    </section>
+  </div>
+)
 
 export default User
