6a2f0e6a83673797515ffacbca76854d49ab9114
Lucas Frois <lucas.frois@lambda3.com.br>
2021-05-27 21:24:08 -0300

    add home page

--- app/src/pages/Home/Home.tsx
+++ app/src/pages/Home/Home.tsx
@@ -1,3 +1,11 @@
-const Home = () => <div>home</div>
+const Home = () => (
+  <div>
+    <h1>Search devs</h1>
+    <span>
+      <input type="text"></input>
+      <button>Buscar</button>
+    </span>
+  </div>
+)
 
 export default Home
